ba23401793705615850e1edc4f63604c3090aced
Sascha Nowak <sascha.nowak@netlogix.de>
2019-01-03 14:02:58 +0100

    [TASK] Reformat code

--- Classes/Service/ImageOptimizer.php
+++ Classes/Service/ImageOptimizer.php
@@ -16,124 +16,125 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class ImageOptimizer implements SingletonInterface
 {
 
-	/**
-	 * @var Logger
-	 */
-	protected $logger;
+    /**
+     * @var Logger
+     */
+    protected $logger;
 
-	/**
-	 * @param FileProcessingService $fileProcessingService
-	 * @param DriverInterface $driver
-	 * @param ProcessedFile $processedFile
-	 * @param File $file
-	 * @param $context
-	 * @param array $configuration
-	 */
-	public function optimizeImage(
-		FileProcessingService $fileProcessingService,
-		DriverInterface $driver,
-		ProcessedFile $processedFile,
-		File $file,
-		$context,
-		array $configuration
-	) {
-		if ($processedFile->getType() === AbstractFile::FILETYPE_IMAGE && $processedFile->isUpdated()) {
-			$this->createWebpImage($processedFile);
-			$this->processImage($processedFile);
-		}
-	}
+    /**
+     * @param FileProcessingService $fileProcessingService
+     * @param DriverInterface $driver
+     * @param ProcessedFile $processedFile
+     * @param File $file
+     * @param $context
+     * @param array $configuration
+     */
+    public function optimizeImage(
+        FileProcessingService $fileProcessingService,
+        DriverInterface $driver,
+        ProcessedFile $processedFile,
+        File $file,
+        $context,
+        array $configuration
+    ) {
+        if ($processedFile->getType() === AbstractFile::FILETYPE_IMAGE && $processedFile->isUpdated()) {
+            $this->createWebpImage($processedFile);
+            $this->processImage($processedFile);
+        }
+    }
 
-	/**
-	 * @param ProcessedFile $processedFile
-	 */
-	protected function processImage(ProcessedFile $processedFile)
-	{
-		$path = CommandUtility::escapeShellArgument(realpath($processedFile->getForLocalProcessing(false)));
-		switch ($processedFile->getMimeType()) {
-			case 'image/jpeg':
-				if (CommandUtility::checkCommand('jpegoptim')) {
-					$command = CommandUtility::getCommand('jpegoptim');
-					$parameters = sprintf('-m85 --strip-all --all-progressive %s', $path);
-					$this->exec($command . ' ' . $parameters . ' 2>&1');
-				}
-				break;
-			case 'image/png':
-				if (CommandUtility::checkCommand('pngquant')) {
-					$command = CommandUtility::getCommand('pngquant');
-					$parameters = sprintf('--force %s --output=%s', $path, $path);
-					$this->exec($command . ' ' . $parameters . ' 2>&1');
-				}
-				if (CommandUtility::checkCommand('optipng')) {
-					$command = CommandUtility::getCommand('optipng');
-					$parameters = sprintf('-i0 -o2 -quiet %s', $path);
-					$this->exec($command . ' ' . $parameters . ' 2>&1');
-				}
-				break;
-			case 'image/gif':
-				if (CommandUtility::checkCommand('gifsicle')) {
-					$command = CommandUtility::getCommand('gifsicle');
-					$parameters = sprintf('-b -O3 %s', $path);
-					$this->exec($command . ' ' . $parameters . ' 2>&1');
-				}
-				break;
-			case 'image/svg+xml':
-				if (CommandUtility::checkCommand('svgo')) {
-					$command = CommandUtility::getCommand('svgo');
-					$parameters = sprintf('--disable=cleanupIDs %s', $path);
-					$this->exec($command . ' ' . $parameters . ' 2>&1');
-				}
-				break;
-			default:
-				return;
-		}
+    /**
+     * @param ProcessedFile $processedFile
+     */
+    protected function processImage(ProcessedFile $processedFile)
+    {
+        $path = CommandUtility::escapeShellArgument(realpath($processedFile->getForLocalProcessing(false)));
+        switch ($processedFile->getMimeType()) {
+            case 'image/jpeg':
+                if (CommandUtility::checkCommand('jpegoptim')) {
+                    $command = CommandUtility::getCommand('jpegoptim');
+                    $parameters = sprintf('-m85 --strip-all --all-progressive %s', $path);
+                    $this->exec($command . ' ' . $parameters . ' 2>&1');
+                }
+                break;
+            case 'image/png':
+                if (CommandUtility::checkCommand('pngquant')) {
+                    $command = CommandUtility::getCommand('pngquant');
+                    $parameters = sprintf('--force %s --output=%s', $path, $path);
+                    $this->exec($command . ' ' . $parameters . ' 2>&1');
+                }
+                if (CommandUtility::checkCommand('optipng')) {
+                    $command = CommandUtility::getCommand('optipng');
+                    $parameters = sprintf('-i0 -o2 -quiet %s', $path);
+                    $this->exec($command . ' ' . $parameters . ' 2>&1');
+                }
+                break;
+            case 'image/gif':
+                if (CommandUtility::checkCommand('gifsicle')) {
+                    $command = CommandUtility::getCommand('gifsicle');
+                    $parameters = sprintf('-b -O3 %s', $path);
+                    $this->exec($command . ' ' . $parameters . ' 2>&1');
+                }
+                break;
+            case 'image/svg+xml':
+                if (CommandUtility::checkCommand('svgo')) {
+                    $command = CommandUtility::getCommand('svgo');
+                    $parameters = sprintf('--disable=cleanupIDs %s', $path);
+                    $this->exec($command . ' ' . $parameters . ' 2>&1');
+                }
+                break;
+            default:
+                return;
+        }
 
-	}
+    }
 
-	/**
-	 * @param ProcessedFile $processedFile
-	 */
-	protected function createWebpImage(ProcessedFile $processedFile)
-	{
-		$path = realpath($processedFile->getForLocalProcessing(false));
-		switch ($processedFile->getMimeType()) {
-			case 'image/jpeg':
-			case 'image/png':
-				if (CommandUtility::checkCommand('cwebp')) {
-					$output = $path . '.webp';
-					$command = CommandUtility::getCommand('cwebp');
-					$parameters = sprintf('-q 85 %s -o %s', CommandUtility::escapeShellArgument($path), CommandUtility::escapeShellArgument($output));
-					$this->exec($command . ' ' . $parameters . ' 2>&1');
-				}
-				break;
-			default:
-				return;
-		}
-	}
+    /**
+     * @param ProcessedFile $processedFile
+     */
+    protected function createWebpImage(ProcessedFile $processedFile)
+    {
+        $path = realpath($processedFile->getForLocalProcessing(false));
+        switch ($processedFile->getMimeType()) {
+            case 'image/jpeg':
+            case 'image/png':
+                if (CommandUtility::checkCommand('cwebp')) {
+                    $output = $path . '.webp';
+                    $command = CommandUtility::getCommand('cwebp');
+                    $parameters = sprintf('-q 85 %s -o %s', CommandUtility::escapeShellArgument($path),
+                        CommandUtility::escapeShellArgument($output));
+                    $this->exec($command . ' ' . $parameters . ' 2>&1');
+                }
+                break;
+            default:
+                return;
+        }
+    }
 
-	/**
-	 * @param string $command
-	 * @return string
-	 */
-	private function exec($command)
-	{
-		$lastOutputLine = CommandUtility::exec($command, $output, $returnValue);
-		if ($returnValue !== 0) {
-			$this->getLogger()->error($lastOutputLine, ['command' => $command]);
-		}
-		return $returnValue;
-	}
+    /**
+     * @param string $command
+     * @return string
+     */
+    private function exec($command)
+    {
+        $lastOutputLine = CommandUtility::exec($command, $output, $returnValue);
+        if ($returnValue !== 0) {
+            $this->getLogger()->error($lastOutputLine, ['command' => $command]);
+        }
+        return $returnValue;
+    }
 
-	/**
-	 * @return Logger
-	 */
-	private function getLogger()
-	{
-		if ($this->logger === null) {
-			$this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(self::class);
-		}
+    /**
+     * @return Logger
+     */
+    private function getLogger()
+    {
+        if ($this->logger === null) {
+            $this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(self::class);
+        }
 
-		return $this->logger;
-	}
+        return $this->logger;
+    }
 
 }
 
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,30 +3,30 @@ defined('TYPO3_MODE') or die();
 
 (function () {
 
-	$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-	$signalSlotDispatcher->connect(
-		\TYPO3\CMS\Core\Resource\ResourceStorage::class,
-		\TYPO3\CMS\Core\Resource\Service\FileProcessingService::SIGNAL_PostFileProcess,
-		\Netlogix\Nximageoptimizer\Service\ImageOptimizer::class,
-		'optimizeImage'
-	);
+    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+    $signalSlotDispatcher->connect(
+        \TYPO3\CMS\Core\Resource\ResourceStorage::class,
+        \TYPO3\CMS\Core\Resource\Service\FileProcessingService::SIGNAL_PostFileProcess,
+        \Netlogix\Nximageoptimizer\Service\ImageOptimizer::class,
+        'optimizeImage'
+    );
 
-	// Extend ImageService to force image processing (https://forge.typo3.org/issues/59067)
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Extbase\Service\ImageService::class] = [
-		'className' => \Netlogix\Nximageoptimizer\Service\ImageService::class
-	];
+    // Extend ImageService to force image processing (https://forge.typo3.org/issues/59067)
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Extbase\Service\ImageService::class] = [
+        'className' => \Netlogix\Nximageoptimizer\Service\ImageService::class
+    ];
 
-	$loggerConfiguration = [];
-	if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9002000) {
-		$loggerConfiguration['logFile'] = \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/nximageoptimizer.log';
-	} else {
-		$loggerConfiguration['logFile'] = 'typo3temp/var/logs/nximageoptimizer.log';
-	}
+    $loggerConfiguration = [];
+    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9002000) {
+        $loggerConfiguration['logFile'] = \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/nximageoptimizer.log';
+    } else {
+        $loggerConfiguration['logFile'] = 'typo3temp/var/logs/nximageoptimizer.log';
+    }
 
-	$GLOBALS['TYPO3_CONF_VARS']['LOG']['Netlogix']['Nximageoptimizer']['writerConfiguration'] = [
-		\TYPO3\CMS\Core\Log\LogLevel::ERROR => [
-			\TYPO3\CMS\Core\Log\Writer\FileWriter::class => $loggerConfiguration
-		],
-	];
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['Netlogix']['Nximageoptimizer']['writerConfiguration'] = [
+        \TYPO3\CMS\Core\Log\LogLevel::ERROR => [
+            \TYPO3\CMS\Core\Log\Writer\FileWriter::class => $loggerConfiguration
+        ],
+    ];
 
 })();
