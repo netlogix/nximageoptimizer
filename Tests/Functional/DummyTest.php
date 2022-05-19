0327fef188c2f737cd94ed31c108f7ee0168f9f2
Alexander Jahn <alexander.jahn@netlogix.de>
2022-05-19 08:25:18 +0200

    PATCH: prepare test environment

---
+++ Tests/Functional/DummyTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nximageoptimizer\Tests\Functional;
+
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+
+class DummyTest extends FunctionalTestCase
+{
+
+    /**
+     * @test
+     * @return void
+     */
+    public function dummyTest()
+    {
+        self::assertTrue(true);
+    }
+}
---
+++ Tests/Unit/DummyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nximageoptimizer\Tests\Unit;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+class DummyTest extends UnitTestCase {
+
+    /**
+     * @test
+     * @return void
+     */
+    public function dummyTest() {
+        self::assertTrue(true);
+    }
+}
