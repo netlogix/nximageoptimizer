735aececa18e18e49ee32b592fe2ff6da4743176
Alexander Jahn <alexander.jahn@netlogix.de>
2022-09-12 09:52:22 +0200

    FEATURE: use FAL API for image manipulation
    This fixes several problems:
    * possible temp files were not cleaned up
    * file metadata in FAL was wrong after images were optimized
    
    Additionally, this introduces support for non-local file storages as a
    side effect.
    
    Another cleanup splits the ImageOptimizer into two distinct
    EventListeners.

---
+++ Classes/EventListener/AbstractImageOptimizer.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nximageoptimizer\EventListener;
+
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Resource\AbstractFile;
+use TYPO3\CMS\Core\Resource\ProcessedFile;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\CommandUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+abstract class AbstractImageOptimizer implements SingletonInterface, LoggerAwareInterface
+{
+	use LoggerAwareTrait;
+
+	protected array $configuration;
+
+	public function __construct()
+	{
+		$this->configuration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('nximageoptimizer');
+	}
+
+	protected function isEnabled(ProcessedFile $processedFile): bool
+	{
+		// this is needed for backwards compatibility
+		// @extensionScannerIgnoreLine
+		if (!(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_FE)) {
+			// this is not needed for TYPO3 backend and would break deferred image processing
+			return false;
+		}
+
+		// Backend introduced a DeferredBackendImageProcessor and creates thumbnails async.
+		// Currently, there is no api to check if the image is processed asynchronously, therefore we disable processing for backend preview images.
+		// https://forge.typo3.org/issues/92188
+		// https://forge.typo3.org/issues/93245
+		if ($processedFile->getTaskIdentifier() === ProcessedFile::CONTEXT_IMAGEPREVIEW) {
+			return false;
+		}
+
+		// if the processed file did not change then there is nothing to do here
+		if (!$processedFile->isUpdated()) {
+			return false;
+		}
+
+		// only optimize images here
+		if ($processedFile->getType() !== AbstractFile::FILETYPE_IMAGE) {
+			return false;
+		}
+
+		return true;
+	}
+
+	protected function exec(string $command): void
+	{
+		$lastOutputLine = CommandUtility::exec($command, $output, $returnValue);
+		if ($returnValue !== 0) {
+			// @extensionScannerIgnoreLine
+			$this->logger->error($lastOutputLine, ['command' => $command]);
+		}
+	}
+}
---
+++ Classes/EventListener/WebpCreator.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nximageoptimizer\EventListener;
+
+use TYPO3\CMS\Core\Resource\Driver\DriverInterface;
+use TYPO3\CMS\Core\Resource\Event\AfterFileProcessingEvent;
+use TYPO3\CMS\Core\Resource\ProcessedFile;
+use TYPO3\CMS\Core\Utility\CommandUtility;
+
+class WebpCreator extends AbstractImageOptimizer
+{
+
+	public function createWebpVersion(AfterFileProcessingEvent $event)
+	{
+		if (!$this->isEnabled($event->getProcessedFile())) {
+			return;
+		}
+
+		$this->createWebpImage($event->getProcessedFile(), $event->getDriver());
+	}
+
+	/**
+	 * Creates a .webp version of the processed image file.
+	 *
+	 * @param ProcessedFile $processedFile
+	 * @param DriverInterface $driver
+	 * @return void
+	 */
+	private function createWebpImage(ProcessedFile $processedFile, DriverInterface $driver): void
+	{
+		if ($this->configuration['disableAutomaticWebpCreation']) {
+			return;
+		}
+
+		$targetFileName = $processedFile->getName() . '.webp';
+		$targetFolder = $processedFile->getParentFolder();
+
+		$localProcessingPath = $processedFile->getForLocalProcessing();
+
+		$path = realpath($localProcessingPath);
+		$targetPath = $path . '.webp';
+		switch ($processedFile->getMimeType()) {
+			case 'image/jpeg':
+			case 'image/png':
+				if (CommandUtility::checkCommand('cwebp')) {
+					$output = $targetPath;
+					$command = CommandUtility::getCommand('cwebp');
+					$parameters = sprintf(
+						'-q 85 %s -o %s',
+						CommandUtility::escapeShellArgument($path),
+						CommandUtility::escapeShellArgument($output)
+					);
+					$this->exec($command . ' ' . $parameters . ' 2>&1');
+				}
+				break;
+			default:
+				return;
+		}
+
+		if ($targetFolder->hasFile($targetFileName)) {
+			$fileIdentifier = $driver->getFileInFolder(
+				$targetFileName,
+				$targetFolder->getIdentifier()
+			);
+			$driver->replaceFile($fileIdentifier, $localProcessingPath);
+		} else {
+			$driver->addFile($localProcessingPath, $targetFolder->getIdentifier(), $targetFileName);
+		}
+	}
+
+}
+
